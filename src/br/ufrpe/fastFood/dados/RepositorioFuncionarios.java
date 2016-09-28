6336153635d328713b6ad44128855573bb5fe0bb
Alessandro Lima <Alessandro Lima@192.168.0.9>
2016-09-28 11:05:55 -0300

    implementação de repositorio

--- src/br/ufrpe/fastFood/dados/RepositorioFuncionarios.java
+++ src/br/ufrpe/fastFood/dados/RepositorioFuncionarios.java
@@ -1,5 +1,22 @@
 package br.ufrpe.fastFood.dados;
+import java.util.ArrayList;
+
+import Teste.Funcionario;
 
 public class RepositorioFuncionarios {
+	private static RepositorioFuncionarios instancia;
+	private ArrayList <Funcionario> listaFuncionarios;
+	
+	private RepositorioFuncionarios(){
+		this.listaFuncionarios = new ArrayList <>();
+	}
+	
+	public static RepositorioFuncionarios getInstancia(){
+		if(instancia == null){
+			instancia = new RepositorioFuncionarios();
+		}
+		
+		return instancia;
+	}
 
 }
