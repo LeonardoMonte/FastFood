2781b330321193453757efc8a2747eb03ae86f5c
carlosazevedo <carlosazevedo@192.168.43.143>
2016-09-28 15:26:06 -0300

    Mudança no repositorio clientes!!

--- src/br/ufrpe/fastFood/dados/RepositorioClientes.java
+++ src/br/ufrpe/fastFood/dados/RepositorioClientes.java
@@ -1,5 +1,27 @@
 package br.ufrpe.fastFood.dados;
+import java.util.ArrayList;
+import Teste.Cliente;
 
 public class RepositorioClientes {
+	
+	private static RepositorioClientes instancia;
+	private ArrayList<Cliente> listaClientes; 
+	
+	private RepositorioClientes(){
+		
+		listaClientes = new ArrayList<>();
+	}
+	
+	public static RepositorioClientes getInstancia(){
+		
+		if(instancia == null){
+			instancia = new RepositorioClientes();
+		}
+		
+		return instancia;
+		
+	}
+	
+	
 
 }
