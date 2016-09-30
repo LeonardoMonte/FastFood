b23d5f83ed59b070e3508151b3e5e8402b93bfa2
carlosazevedo <azevedo1236@gmail.com>
2016-09-30 20:03:33 -0300

    repositorio...

--- src/br/ufrpe/fastFood/dados/RepositorioFuncionarios.java
+++ src/br/ufrpe/fastFood/dados/RepositorioFuncionarios.java
@@ -82,33 +82,4 @@ public class RepositorioFuncionarios {
 			}
 		}
 	}
-	
-	public void alterarFuncionario(int opcao, Funcionario f){
-		
-		switch (opcao) {
-		
-		case 1:
-			
-			break;
-			
-		case 2:
-			
-			break;
-			
-		case 3:
-				
-			break;
-			
-		case 4:
-			
-			break;
-
-		default:
-			
-			System.out.println("Opção invalida!!!");
-			
-			break;
-		}
-		
-	}
 }
