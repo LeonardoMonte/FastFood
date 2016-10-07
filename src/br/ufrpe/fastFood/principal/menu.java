20ab985cd45b05ff1303e4da158ca28086a34361
carlosazevedo <azevedo1236@gmail.com>
2016-10-07 13:06:31 -0300

    Ajuste no login do cliente, foi adicionado uma tela logo apos o login.

--- src/br/ufrpe/fastFood/principal/menu.java
+++ src/br/ufrpe/fastFood/principal/menu.java
@@ -33,92 +33,125 @@ public class menu {
 
 			case 1:
 
-				boolean aux2 = false;
+				int aux2 = 0;
 
-				System.out.println("-----------Auto Atendimento----------\n");
-				System.out.println("1 - Cadastro;");
-				System.out.println("2 - Login;");
-				System.out.println("3 - Voltar;\n");
-				System.out.print("Digite sua opção: ");
+				while (aux2 != 3) {
+					
+					System.out.println("-----------Auto Atendimento----------\n");
+					System.out.println("1 - Cadastro;");
+					System.out.println("2 - Login;");
+					System.out.println("3 - Voltar;\n");
+					System.out.print("Digite sua opção: ");
+					
+					opcao = in.nextInt();
+					
+					switch (opcao) {
 
-				opcao = in.nextInt();
+					case 1:
 
-				switch (opcao) {
+						System.out.print("------------Cadastro Cliente---------- \n");
 
-				case 1:
+						System.out.print("Nome: ");
+						String nome = in.nextLine();
+						in.nextLine();
 
-					System.out.print("------------Cadastro Cliente---------- \n");
-					System.out.print("Nome: ");
-					String nome = in.nextLine();
-					in.nextLine();
-					System.out.print("CPF: ");
-					String id = in.nextLine();
-					System.out.print("Nascimento: ");
-					String nascimento = in.nextLine();
-					System.out.print("Rua: ");
-					String rua = in.nextLine();
-					System.out.print("Numero: ");
-					int numero = in.nextInt();
-					System.out.print("Bairro: ");
-					String bairro = in.nextLine();
-					in.nextLine();
-					System.out.print("Cidade: ");
-					String cidade = in.nextLine();
-					System.out.print("Estado: ");
-					String estado = in.nextLine();
-					System.out.print("Telefone: ");
-					String telefone = in.nextLine();
-					System.out.print("Email: ");
-					String email = in.nextLine();
-					System.out.print("Senha: ");
-					String senha = in.nextLine();
+						System.out.print("CPF: ");
+						String id = in.nextLine();
 
-					Endereco end1 = new Endereco(rua, bairro, cidade, estado, numero, telefone);
-					Cliente cliente1 = new Cliente(nome, id, nascimento, end1, senha, email);
-					repositorioClientes.cadastrarCliente(cliente1);
+						System.out.print("Nascimento: ");
+						String nascimento = in.nextLine();
 
-					break;
+						System.out.print("Rua: ");
+						String rua = in.nextLine();
 
-				case 2:
+						System.out.print("Numero: ");
+						int numero = in.nextInt();
 
-					boolean play = false;
+						System.out.print("Bairro: ");
+						String bairro = in.nextLine();
+						in.nextLine();
 
-					while (!play) {
+						System.out.print("Cidade: ");
+						String cidade = in.nextLine();
 
-						System.out.println("------------Login Cliente------------\n");
-						System.out.println("Digite seu ID: ");
-						String tempId = in.nextLine();
-						System.out.println("Digite sua senha: ");
-						String tempSenha = in.nextLine();
+						System.out.print("Estado: ");
+						String estado = in.nextLine();
 
-						Cliente auxId = repositorioClientes.buscarCliente(tempId);
+						System.out.print("Telefone: ");
+						String telefone = in.nextLine();
 
-						if ((auxId != null) && (auxId.getSenha().contains(tempSenha))) {
-							play = true;
+						System.out.print("Email: ");
+						String email = in.nextLine();
 
-							// Continuação do codigo...
+						System.out.print("Senha: ");
+						String senha = in.nextLine();
 
-						} else {
+						Endereco end1 = new Endereco(rua, bairro, cidade, estado, numero, telefone);
+						Cliente cliente1 = new Cliente(nome, id, nascimento, end1, senha, email);
+						repositorioClientes.cadastrarCliente(cliente1);
 
-							System.out.println("O id ou a senha foi digitado incorretamente, tente novamente!");
+						break;
+
+					case 2:
+
+						boolean play = false;
+
+						while (!play) {
+
+							System.out.println("------------Login Cliente------------\n");
+
+							System.out.println("Digite seu ID: ");
+							String tempId = in.nextLine();
+
+							System.out.println("Digite sua senha: ");
+							String tempSenha = in.nextLine();
+
+							Cliente auxId = repositorioClientes.buscarCliente(tempId);
+
+							if ((auxId != null) && (auxId.getSenha().equals(tempSenha))) {
+								play = true;
+								
+								System.out.println("-------Seja Bem-Vindo " + auxId.getNome() + "-------");
+								System.out.println("1 - Fazer um pedido");
+								System.out.println("2 - Alterar senha");
+								System.out.println("3 - Sair");
+								System.out.println("\nDigite sua opção: ");
+								
+								opcao = in.nextInt();
+								
+								switch (opcao) {
+								
+								case 1:
+									
+									// Continuação do codigo...
+									
+									break;
+
+								default:
+									
+									System.out.print("Opção invalida!");
+									
+									break;
+								}
+
+							} else {
+
+								System.out.println("O id ou a senha foi digitado incorretamente, tente novamente!");
+							}
 						}
+
+						break;
+
+					default:
+
+						System.out.println("Opção invalida!");
+
+						break;
 					}
-
-					break;
-
-				case 3:
-
-					break;
-
-				default:
-
-					System.out.println("Opção invalida!");
-
+					
 					break;
 				}
-
-				break;
-
+				
 			case 2:
 
 				System.out.println("-----------Caixa----------\n");
@@ -146,7 +179,7 @@ public class menu {
 						System.out.println("Digite sua senha: ");
 						String tempSenha = in.nextLine();
 						Funcionario auxId = repositorioFuncionario.buscarFuncionario(tempId);
-						if ((auxId != null) && (auxId.getSenha().contains(tempSenha))) {
+						if ((auxId != null) && (auxId.getSenha().equals(tempSenha))) {
 							play = true;
 
 							// Continuação do codigo...
@@ -183,11 +216,14 @@ public class menu {
 				break;
 
 			default:
+				
+				System.out.println("Opção invalida!");
 
 				break;
 			}
 
 		}
+		
 		in.close();
 	}
 }
