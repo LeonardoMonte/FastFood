932aebe7551c5dc55be8869aa199fd785c78fea6
carlosazevedo <azevedo1236@gmail.com>
2016-10-07 15:13:38 -0300

    Alteração na parte do administrador!

--- src/br/ufrpe/fastFood/principal/menu.java
+++ src/br/ufrpe/fastFood/principal/menu.java
@@ -1,9 +1,11 @@
 package br.ufrpe.fastFood.principal;
-
 import java.util.Scanner;
+
+import br.ufrpe.fastFood.beans.Admin;
 import br.ufrpe.fastFood.beans.Cliente;
 import br.ufrpe.fastFood.beans.Endereco;
 import br.ufrpe.fastFood.beans.Funcionario;
+import br.ufrpe.fastFood.beans.Produto;
 import br.ufrpe.fastFood.dados.RepositorioClientes;
 import br.ufrpe.fastFood.dados.RepositorioFuncionarios;
 import br.ufrpe.fastFood.dados.RepositorioProdutos;
@@ -20,6 +22,7 @@ public class menu {
 		RepositorioFuncionarios repositorioFuncionario = RepositorioFuncionarios.getInstancia();
 		RepositorioProdutos repositorioProdutos = RepositorioProdutos.getInstancia();
 		RepositorioVendas repositorioVendas = RepositorioVendas.getInstancia();
+		Admin admin = new Admin("Carlos", "123456", "Admin123", "123");
 		
 
 		Scanner in = new Scanner(System.in);
@@ -90,19 +93,19 @@ public class menu {
 						
 						String senha;
 						String senha2;
-						int tentativa = 0;
 						
 						do {
 							
 							System.out.print("Senha: ");
 							senha = in.nextLine();
+							in.nextLine();
 							
 							System.out.print("Confime a Senha: ");
 							senha2 = in.nextLine();
+							in.nextLine();
 							
-							tentativa++;
 							
-						} while (senha.equals(senha2) && (tentativa < 3));
+						} while ((senha.equals(senha2) != true));
 						
 						Endereco end1 = new Endereco(rua, bairro, cidade, estado, numero, telefone);
 						Cliente cliente1 = new Cliente(nome, id, nascimento, end1, senha, email);
@@ -118,10 +121,11 @@ public class menu {
 
 							System.out.println("------------Login Cliente------------\n");
 
-							System.out.println("Digite seu ID: ");
+							System.out.print("Digite seu ID: ");
 							String tempId = in.nextLine();
+							in.nextLine();
 
-							System.out.println("Digite sua senha: ");
+							System.out.print("Digite sua senha: ");
 							String tempSenha = in.nextLine();
 
 							Cliente auxCliente = repositorioClientes.buscarCliente(tempId);
@@ -134,7 +138,7 @@ public class menu {
 								System.out.println("1 - Fazer um pedido");
 								System.out.println("2 - Alterar senha");
 								System.out.println("3 - Sair");
-								System.out.println("\nDigite sua opção: ");
+								System.out.print("\nDigite sua opção: ");
 								
 								opcao = in.nextInt();
 								
@@ -143,7 +147,7 @@ public class menu {
 								case 1:
 									
 									System.out.println("----------Produtos----------\n" + repositorioProdutos.listarProdutos());
-									
+									in.nextLine();
 									
 									break;
 
@@ -184,6 +188,148 @@ public class menu {
 				switch (opcao) {
 
 				case 1:
+					
+					System.out.println("------------Login Administrador------------\n");
+					
+					System.out.println("Digite seu ID: ");
+					String tempIdAdmin = in.nextLine();
+					in.nextLine();
+					
+					System.out.println("Digite sua senha: ");
+					String tempSenhaAdmin = in.nextLine();
+					
+					if((admin.getId().equals(tempIdAdmin) == true) && (admin.getSenha().equals(tempSenhaAdmin) == true)){
+						
+						System.out.println("-----------Bem-Vindo-----------");
+						System.out.println("1 - Adicionar funcionario");
+						System.out.println("2 - Remover Funcionario");
+						System.out.println("3 - listar Funcionarios");
+						System.out.println("4 - Adicionar Produtos");
+						System.out.println("5 - Remover Produto");
+						System.out.println("6 - Listar Produtos");
+						
+						opcao = in.nextInt();
+						
+						switch (opcao) {
+						
+						case 1:
+							
+							System.out.print("------------Cadastro Cliente---------- \n");
+
+							System.out.print("Nome: ");
+							String nome = in.nextLine();
+							in.nextLine();
+
+							System.out.print("CPF: ");
+							String id = in.nextLine();
+
+							System.out.print("Nascimento: ");
+							String nascimento = in.nextLine();
+
+							System.out.print("Rua: ");
+							String rua = in.nextLine();
+
+							System.out.print("Numero: ");
+							int numero = in.nextInt();
+
+							System.out.print("Bairro: ");
+							String bairro = in.nextLine();
+							in.nextLine();
+
+							System.out.print("Cidade: ");
+							String cidade = in.nextLine();
+
+							System.out.print("Estado: ");
+							String estado = in.nextLine();
+
+							System.out.print("Telefone: ");
+							String telefone = in.nextLine();
+							
+							String senha;
+							String senha2;
+							
+							do {
+								
+								System.out.print("Senha: ");
+								senha = in.nextLine();
+								in.nextLine();
+								
+								System.out.print("Confime a Senha: ");
+								senha2 = in.nextLine();
+								in.nextLine();
+																
+							} while ((senha.equals(senha2) != true));
+							
+							Endereco end1 = new Endereco(rua, bairro, cidade, estado, numero, telefone);
+							Funcionario funcionario1 = new Funcionario(nome, id, nascimento, end1, senha);
+							repositorioFuncionario.cadastrarFuncionario(funcionario1);
+							
+							break;
+							
+						case 2:
+							
+							System.out.println("Digite o ID do Funcionario: ");
+							String auxIdFunc = in.nextLine();
+							in.nextLine();
+							
+							if(repositorioFuncionario.buscarFuncionario(auxIdFunc).getId().equals(auxIdFunc)){
+								
+								repositorioFuncionario.removerFuncionario(auxIdFunc);
+							}
+							
+							break;
+							
+						case 3:
+							
+							repositorioFuncionario.listarFuncinario();
+							in.hasNext();
+							
+							break;
+							
+						case 4:
+							
+							System.out.print("Nome do Produto: ");
+							String nomeProd = in.nextLine();
+							
+							System.out.print("Codigo do Produto: ");
+							String codigoProd = in.nextLine();
+							
+							System.out.print("Valor do Produto: ");
+							Double valorProd = in.nextDouble();
+							
+							Produto produto1 = new Produto(nomeProd, valorProd, codigoProd);
+							repositorioProdutos.cadastrar(produto1);
+							
+							break;
+							
+						case 5:
+							
+							System.out.println("Digite o Codigo do Produto: ");
+							String auxCodigoProd = in.nextLine();
+							in.nextLine();
+							
+							if(repositorioProdutos.buscarProduto(auxCodigoProd).getCodigo().equals(auxCodigoProd)){
+								
+								repositorioProdutos.removerProduto(auxCodigoProd);
+							}
+							
+							break;
+							
+						case 6:
+							
+							repositorioProdutos.listarProdutos();
+							in.nextLine();
+							
+							break;
+
+						default:
+							
+							System.out.println("Opção invalida!");
+							
+							break;
+						}
+						
+					}
 
 					break;
 
@@ -197,12 +343,13 @@ public class menu {
 						
 						System.out.println("Digite seu ID: ");
 						String tempId = in.nextLine();
+						in.nextLine();
 						
 						System.out.println("Digite sua senha: ");
 						String tempSenha = in.nextLine();
 						
-						Funcionario auxId = repositorioFuncionario.buscarFuncionario(tempId);
-						if ((auxId != null) && (auxId.getSenha().equals(tempSenha))) {
+						Funcionario auxFuncionario = repositorioFuncionario.buscarFuncionario(tempId);
+						if ((auxFuncionario != null) && (auxFuncionario.getSenha().equals(tempSenha))) {
 							play = true;
 
 							// Continuação do codigo...
