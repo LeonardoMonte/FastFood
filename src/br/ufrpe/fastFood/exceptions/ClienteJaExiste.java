852fa82a585de9092f22fb42a9947a732a66e41d
Alessandro Lima <Alessandro Lima@Alessandro>
2017-02-08 14:09:30 -0300

    Adicionada duas excessoes ao pacote

---
+++ src/br/ufrpe/fastFood/exceptions/ClienteJaExiste.java
@@ -0,0 +1,16 @@
+package br.ufrpe.fastFood.exceptions;
+
+public class ClienteJaExiste extends Exception {
+
+	private String idCliente;
+
+	public ClienteJaExiste(String idCliente){
+		super("Cliente já cadastrado no sistema.");
+		this.idCliente = idCliente;
+	}
+
+	public String getIdCliente(){
+		return this.idCliente;
+	}
+
+}
